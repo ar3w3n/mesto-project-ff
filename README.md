352a64a209bf6b7aeabfd4ad2e4fbde9ce643126
Egor Minakov <minakov.egor.00@yandex.ru>
2024-07-31 00:32:52 +0300

    first ver

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Проектная работа Mesto
+https://ar3w3n.github.io/mesto-project-ff/
 
+https://github.com/ar3w3n/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const container = document.querySelector('.content')
+const cardContainer = container.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+const createCard = (link, name) => {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  const cardItem = deleteButton.closest('.card');
+  deleteButton.addEventListener('click', () => deleteCard(cardItem))
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+  return cardElement;
+};
+
 // @todo: Функция удаления карточки
+const deleteCard = (cardItem) => {
+  cardItem.remove();
+};
 
 // @todo: Вывести карточки на страницу
+
+const renderCards = () => {
+  initialCards.forEach((element) => {
+    cardContainer.append(createCard(element.link, element.name));
+  });
+};
+
+renderCards();
